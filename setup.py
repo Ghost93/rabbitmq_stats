db543363321e745175430a1895304e2f38618ac1
MC <mcronce@sevone.com>
2017-08-17 16:23:18 +0100

    setup.py:  Support Python 3.4

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ setuptools.setup(
 		'Natural Language :: English',
 		'Programming Language :: Python :: 2.6',
 		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 3.4',
 	]
 )
 
