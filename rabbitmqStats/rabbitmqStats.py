3d9822187ac0bec6faf4977507aa868938523da7
MC <mcronce@sevone.com>
2017-08-17 13:05:58 +0100

    rabbitmqStats/rabbitmqStats.py:  Tabs and blank lines

--- rabbitmqStats/rabbitmqStats.py
+++ rabbitmqStats/rabbitmqStats.py
@@ -7,84 +7,80 @@ import sys
 
 
 class rabbitmqStats():
+	"""This is the class docstring"""
+	# defined array to hold queue names
+	r = []
+	timestamp = ""
 
-    """This is the class docstring"""
+	def __init__(self, host, port, url, user, pwd):
+		# Get epoch. can be formatted later
+		self.timestamp = int(time.time())
 
-    # defined array to hold queue names
-    r = []
-    timestamp = ""
+		try:
+			self.r = requests.get(
+				'http://{0}:{1}{2}'.format(host, port, url), auth=(user, pwd)
+			)
 
-    def __init__(self, host, port, url, user, pwd):
+		except Exception as e:
+			print("init: {0}".format(e))
+			sys.exit()
 
-        # Get epoch. can be formatted later
-        self.timestamp = int(time.time())
+	def fetch(self, host, port, url, user, pwd):
+		return self.__init__(host, port, url, user, pwd)
 
-        try:
-            self.r = requests.get(
-                'http://{0}:{1}{2}'.format(host, port, url), auth=(user, pwd)
-            )
+	def queue_names(self):
+		"""Get queue names from rabbitmq and return a list"""
+		qs = []
 
-        except Exception as e:
-            print("init: {0}".format(e))
-            sys.exit()
+		{qs.append((str(queue['name']))) for queue in self.r.json()}
 
-    def fetch(self, host, port, url, user, pwd):
-        return self.__init__(host, port, url, user, pwd)
+		return qs
 
-    def queue_names(self):
-        """Get queue names from rabbitmq and return a list"""
-        qs = []
+	def __walk_json(self, node):
+		stat_data = {}
+		for key, item in node.items():
+			if isinstance(item, dict):
+				if 'rate' in item:
+					stat_data[str(key)] = int(item['rate'])
+				else:
+					self.__walk_json(item)
+			else:
+				stat_data[str(key)] = int(item)
 
-        {qs.append((str(queue['name']))) for queue in self.r.json()}
+		return stat_data
 
-        return qs
+	def queue_msg_stats(self, name):
+		"""Get metrics of rabbitmq queue"""
+		num = self.queue_names().index(name)
 
-    def __walk_json(self, node):
-        stat_data = {}
-        for key, item in node.items():
-            if isinstance(item, dict):
-                if 'rate' in item:
-                    stat_data[str(key)] = int(item['rate'])
-                else:
-                    self.__walk_json(item)
-            else:
-                stat_data[str(key)] = int(item)
+		queue = self.r.json()[num]
 
-        return stat_data
-
-    def queue_msg_stats(self, name):
-        """Get metrics of rabbitmq queue"""
-        num = self.queue_names().index(name)
-
-        queue = self.r.json()[num]
-
-        if 'message_stats' in queue:
-            msg_stats = queue['message_stats']
-            cleaned_msg_stats = self.__walk_json(msg_stats)
-            cleaned_msg_stats['name'] = name
-            return cleaned_msg_stats
-        else:
-            return "Queue '{}' does not have "\
-                "any 'message_stats' available".format(name)
+		if 'message_stats' in queue:
+			msg_stats = queue['message_stats']
+			cleaned_msg_stats = self.__walk_json(msg_stats)
+			cleaned_msg_stats['name'] = name
+			return cleaned_msg_stats
+		else:
+			return "Queue '{}' does not have "\
+				"any 'message_stats' available".format(name)
 
 
 if __name__ == "__main__":
+	internal_rbq = rabbitmqStats('localhost', '15672', '/api/queues', 'guest', 'guest')
 
-    internal_rbq = rabbitmqStats('localhost',
-                                 '15672', '/api/queues', 'guest', 'guest')
+	names = internal_rbq.queue_names()
 
-    names = internal_rbq.queue_names()
+	print("Queue Names: \n{}\n".format(names))
+	print("Queue Stats:")
 
-    print("Queue Names: \n{}\n".format(names))
-    print("Queue Stats:")
+	for i in names:
+		stats = internal_rbq.queue_msg_stats(i)
+		if type(stats) is dict:
+			stats['timestamp'] = time.strftime(
+				'%a %d %b %H:%M:%S %Z %Y',
+				time.localtime(internal_rbq.timestamp)
+			)
+			print("{}\n".format(stats))
+		else:
+			print("{}\n".format(stats))
 
-    for i in names:
-        stats = internal_rbq.queue_msg_stats(i)
-        if type(stats) is dict:
-            stats['timestamp'] = time.strftime(
-                '%a %d %b %H:%M:%S %Z %Y',
-                time.localtime(internal_rbq.timestamp)
-            )
-            print("{}\n".format(stats))
-        else:
-            print("{}\n".format(stats))
